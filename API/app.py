e33797a446b8f572dd17ca959546c2980ae872d3
SANYAM1996 <sanyamsingh.96@gmail.com>
2026-07-16 20:10:49 +0100

    Allow Azure Static Web App

--- API/app.py
+++ API/app.py
@@ -24,6 +24,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "http://localhost:5174",
+        "https://mango-pebble-062307503.7.azurestaticapps.net",
     ],
     allow_credentials=True,
     allow_methods=["*"],
